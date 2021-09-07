b64f22cd4dc874f22f8adbbdd5ec975b2f6dcb92
HitCoder9768 <hitkid61@gmail.com>
2021-09-07 16:26:13 +0100

    added custom measurements to hp and iem graph tools, plus custom targets

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuration options
-const init_phones = ["MH755"],                      // Optional. Which graphs to display on initial load. Note: Share URLs will override this set
+const init_phones = ["Aria"],                      // Optional. Which graphs to display on initial load. Note: Share URLs will override this set
       DIR = "data/",                                // Directory where graph files are stored
       data_format = "other",                   // Accepts "AudioTools," "REW," or "other"
       default_channels = ["L","R"],                 // Which channels to display. Avoid javascript errors if loading just one channel per phone
